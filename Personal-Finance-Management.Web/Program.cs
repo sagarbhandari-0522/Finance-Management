7457af124fbdc2f05777bc6444ed1ca391ad3395
Sagar Bhandari <sagarvandari1@gmail.com>
2025-06-24 19:45:57 +1200

    feat: configure database

--- Personal-Finance-Management.Web/Program.cs
+++ Personal-Finance-Management.Web/Program.cs
@@ -1,9 +1,21 @@
+
+using Microsoft.EntityFrameworkCore;
+using Personal_Finance_Management.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Configure EFCore
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultCOnnection")));
 
 var app = builder.Build();
+//using (var scope=app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

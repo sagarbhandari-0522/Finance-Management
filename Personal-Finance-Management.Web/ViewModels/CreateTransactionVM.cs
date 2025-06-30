a82070d0cc85bef3d45d86c2facccf079a4380fb
Sagar Bhandari <sagarvandari1@gmail.com>
2025-06-30 17:36:09 +1200

    feat: index and create action of transaction

---
+++ Personal-Finance-Management.Web/ViewModels/CreateTransactionVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Finance_Management.Web.ViewModels
+{
+    public class CreateTransactionVM
+    {
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
+        public decimal Amount { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime? TransactionAt { get; set; }
+        public List<SelectListItem>? CategoryList { get; set; }
+    }
+}

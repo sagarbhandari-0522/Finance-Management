0301bbee5f22e81177eeac15e8bf1f32de7ddca3
Sagar Bhandari <sagarvandari1@gmail.com>
2025-07-01 12:36:51 +1200

    feat: add application user model and update database

--- Personal-Finance-Management.Infrastructure/Migrations/20250701002919_add_user_table.Designer.cs
+++ Personal-Finance-Management.Infrastructure/Migrations/20250701002919_add_user_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Personal_Finance_Management.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Personal_Finance_Management.Infrastructure.Data;
 namespace Personal_Finance_Management.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250701002919_add_user_table")]
+    partial class add_user_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

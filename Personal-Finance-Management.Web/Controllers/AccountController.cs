83a0f5519f07ff0e398625e1906f93d838594abe
Sagar Bhandari <sagarvandari1@gmail.com>
2025-07-02 15:32:48 +1200

    feat: add firstname in claims to access it in view

--- Personal-Finance-Management.Web/Controllers/AccountController.cs
+++ Personal-Finance-Management.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Personal_Finance_Management.Domain.Entities;
 using Personal_Finance_Management.Web.ViewModels;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Personal_Finance_Management.Web.Controllers
@@ -26,13 +27,17 @@ namespace Personal_Finance_Management.Web.Controllers
             var user = new ApplicationUser
             {
                 FirstName = model.FirstName,
-                LastName=model.LastName,
+                LastName = model.LastName,
                 Email = model.Email,
                 UserName = model.Email
             };
             var result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
+                var existingClaims = await _userManager.GetClaimsAsync(user);
+                var existingFirstNameClaim = existingClaims.FirstOrDefault(c => c.Type == "FirstName");
+                if (existingFirstNameClaim != null) await _userManager.RemoveClaimAsync(user, existingFirstNameClaim);
+                await _userManager.AddClaimAsync(user, new Claim("FirstName", user.FirstName));
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction("Index", "Home");
             }
@@ -41,25 +46,30 @@ namespace Personal_Finance_Management.Web.Controllers
             return View(model);
         }
         [HttpGet]
-        public IActionResult Login() =>  View();
+        public IActionResult Login() => View();
         [HttpPost]
         public async Task<IActionResult> Login(UserLoginVM model)
         {
             if (!ModelState.IsValid) return View(model);
-            var result=await _signInManager.PasswordSignInAsync(
-                userName:model.Email,
-                password:model.Password,
-                isPersistent:model.RememberMe,
-                lockoutOnFailure:false
+            var result = await _signInManager.PasswordSignInAsync(
+                userName: model.Email,
+                password: model.Password,
+                isPersistent: model.RememberMe,
+                lockoutOnFailure: false
                 );
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                var existingClaims = await _userManager.GetClaimsAsync(user);
+                var existingFirstNameClaim = existingClaims.FirstOrDefault(c => c.Type == "FirstName");
+                if (existingFirstNameClaim != null) await _userManager.RemoveClaimAsync(user, existingFirstNameClaim);
+                await _userManager.AddClaimAsync(user, new Claim("FirstName", user.FirstName));
                 return RedirectToAction("Index", "Home");
             }
             ModelState.AddModelError(string.Empty, "Invalid Login attempt");
             return View(model);
         }
-      
+
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
